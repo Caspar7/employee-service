9758b0e1df966c58f8b93d9e96ca390d9f6c3972
Yunchao Du <ycdu@thoughtworks.com>
2019-02-18 21:05:15 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,22 @@ randPort(){
     echo $(($num%$max+$min))
 }
 
+echo "stop and delete exist docker images and container..."
+running=`docker ps | grep ${serviceName} | awk '{print $1}'`
+if [ ! -z "$running" ]; then
+    docker stop $running
+fi
+
+container=`docker ps -a | grep ${serviceName} | awk '{print $1}'`
+if [ ! -z "$container" ]; then
+    docker rm $container -f
+fi
+
+imagesid=`docker images|grep -i ${serviceName}|awk '{print $3}'`
+if [ ! -z "$imagesid" ]; then
+    docker rmi $imagesid -f
+fi
+
 echo "scp docker image to target server"
 src_dir=${images_path}${serviceName}_${BUILD_NUMBER}.tar
 dest_dir=${images_path}
@@ -24,11 +40,12 @@ password=root
 echo "deploy docker container..."
 deployPort=$(randPort 10000 60000)
 
-
+runCmd="docker run --env env=uat --env deployIp=${deployIp} --env deployPort=${deployPort} -it -d -p ${deployPort}:${deployPort} --name ${serviceName} ${serviceName}:${BUILD_NUMBER}"
 if [ "local" = "${env}" ];then
-    docker run --env env=uat deployPort=${deployPort} -it -d -p ${deployPort}:${deployPort} --name ${serviceName} ${serviceName}:${BUILD_NUMBER}
+    ${runCmd}
     exit 0
 fi
+
 
 # scp docker file to remote server
 #./expect_scp.sh $host $port $username $password $src_dir $dest_dir
