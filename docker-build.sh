d9c954787022c74d104ab69c6682b8c1fc42ab54
Yunchao Du <ycdu@thoughtworks.com>
2019-02-19 14:25:00 +0800

    update docker build

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-./config.sh
 serviceName="employee-service"
 BUILD_NUMBER=$1
 
-echo "start build docker image..."
+echo "start build docker image ..."
 docker build -t ${serviceName}:${BUILD_NUMBER} .
+
+echo "create docker images dir ..."
+mkdir -p ./build/images/${serviceName}/${BUILD_NUMBER}
+
+echo "save docker image ${serviceName}_${BUILD_NUMBER}.tar ..."
+docker save -o ./build/images/${serviceName}_${BUILD_NUMBER}.tar ${serviceName}:${BUILD_NUMBER}
+cp ./docker-deploy.sh ./build/images/${serviceName}/${BUILD_NUMBER}
