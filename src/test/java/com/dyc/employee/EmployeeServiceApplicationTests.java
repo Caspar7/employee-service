51265607f6b40e69a611227bfd2d833af77782e8
Yunchao Du <ycdu@thoughtworks.com>
2019-02-21 16:52:40 +0800

    add profie to test

--- src/test/java/com/dyc/employee/EmployeeServiceApplicationTests.java
+++ src/test/java/com/dyc/employee/EmployeeServiceApplicationTests.java
@@ -2,13 +2,13 @@ package com.dyc.employee;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ConfigurationProperties(prefix = "dev")
+@ActiveProfiles("dev")
 public class EmployeeServiceApplicationTests {
 
     @Test
